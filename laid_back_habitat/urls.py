a9c44b2fb43dc94910265c1de49d58dfcbb30b61
dwatow <dwatow@gmail.com>
2016-04-07 04:04:02 +0000

    add README.md, index page

--- laid_back_habitat/urls.py
+++ laid_back_habitat/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from laid_back_habitat.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', index),
 ]
