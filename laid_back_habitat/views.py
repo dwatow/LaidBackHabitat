7f32c5a3ed11f96a614842a420d1d0adfe1d4116
dwatow <dwatow@gmail.com>
2016-05-08 13:16:27 +0000

    add calendar for select date by jquery

--- laid_back_habitat/views.py
+++ laid_back_habitat/views.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
+import time
+import datetime
+
 def index(request):
     return render_to_response('index.html',locals())
     
 def order_room(request):
-    if 'order' in request.POST:
-        year = request.POST.get('year', '')
-        month = request.POST.get('month', '')
-        day = request.POST.get('day', '')
-        return HttpResponse('客人您於'+year+'年'+month+'月'+day+'日'+'，有訂房。')
+    from_str = request.POST.get('from', '')
+    to_str = request.POST.get('to', '')
+    if from_str and to_str:
+        from_year, from_month, from_day = request.POST.get('from', '').split('-')
+        from_datetime = datetime.datetime(int(from_year), int(from_month), int(from_day))  
+
+        to_year, to_month, to_day = request.POST.get('to', '').split('-')
+        to_datetime = datetime.datetime(int(to_year), int(to_month), int(to_day))  
+
+        total_day = abs((to_datetime - from_datetime).days)
+        return HttpResponse('客人您於' + str(from_datetime.date()) + '到' + str(to_datetime.date()) + '，有訂房。<br />祝你這' + str(total_day) + '天玩得開心。')
     else:
-        HttpResponse('您要訂的房間已滿。')
+        return HttpResponse('您要兩個時都輸入，才能知道您訂房的狀況。')
         #return HttpResponseRedirect('/')
