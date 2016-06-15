c8778dc5518705d348b47c76dcc3170f5fa7b58e
dwatow <dwatow@gmail.com>
2016-06-15 15:52:51 +0800

    fix output string

--- orderRoom/MyLib/MyDateTime.py
+++ orderRoom/MyLib/MyDateTime.py
@@ -5,23 +5,23 @@ import datetime
 class MyDateTime:
     def __init__(self, yyyy_mm_dd):
         year, month, day = yyyy_mm_dd.split('-')
-        self.main_datetime = datetime.datetime(int(year), int(month), int(day))  
+        self.main_datetime = datetime.datetime(int(year), int(month), int(day))
 
     def date(self):
         return self.main_datetime.date()
-        
+
     def date_time(self):
         return self.main_datetime
-        
+
     def time(self):
         return self.main_datetime.time()
-    
+
     def add_day(self, day_num):
         return self.main_datetime + datetime.timedelta(days=day_num)
 
     def comprise_between(self, bound_my_datetime):
         return abs((bound_my_datetime.main_datetime - self.main_datetime).days)+1
-        
+
     def comprise_everyday(self, bound_my_datetime):
         everyday_list=[]
         for day_counter in range(self.comprise_between(bound_my_datetime)):
@@ -30,4 +30,4 @@ class MyDateTime:
         return everyday_list
 
     def __str__(self):
-        return self.main_datetime
+        return str(self.main_datetime)
